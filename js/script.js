dc2eba7a382e594d9397969c6e0077ad7549baab
FrancescoGiommi <francescogiommi97@icloud.com>
2024-10-29 18:21:07 +0100

    Add card to js, id outptcard and change id html element in class

--- js/script.js
+++ js/script.js
@@ -47,14 +47,29 @@ const teamMembers = [
 ];
 
 /* dichiaro le classi degli elementi */
-const profileImage = document.getElementById("image");
-const profileName = document.getElementById("name");
-const profileRole = document.getElementById("role");
-const profileEmail = document.getElementById("email");
-
-const imageProfile = profileImage.value;
-const nameProfile = profileName.value;
-const roleProfile = profileRole.value;
-const emailProfile = profileEmail.value;
-
-console.log(imageProfile, nameProfile, roleProfile, emailProfile);
+const cardOutput = document.getElementById("output-card");
+let cardHtml = "";
+teamMembers.forEach((member) => {
+  const memberCard = `
+  ù<div class="card mb-3" style="max-width: 540px">
+            <div class="row g-0">
+              <div class="col-md-4">
+                <img
+                  src="..."
+                  class="img-fluid rounded-start image"
+                  alt="..."
+                />
+              </div>
+              <div class="col-md-8">
+                <div class="card-body">
+                  <h2 class="card-title h5 name">Nome e Cognome</h2>
+                  <span class="card-text d-block role">Ruolo</span>
+                  <span class="card-text email">Email</span>
+                </div>
+              </div>
+            </div>
+          </div>
+  `;
+  cardHtml += memberCard;
+});
+cardOutput.innerHTML = cardHtml;
